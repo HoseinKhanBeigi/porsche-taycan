90697de46097e5dbaad155bf8f9b05846656920f
MAC <mac@MACs-iMac.local>
2024-02-14 00:09:06 +0330

    add couple of svg elements to pixiComponent

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import SvgRotatingLine from "./PixiComponent.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <SvgRotatingLine />
